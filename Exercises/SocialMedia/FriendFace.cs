ac3f16d46637a3c29466f77225b7c479f9bc5d36
Lillie <75392499+iLillie@users.noreply.github.com>
2022-03-21 10:39:31 +0100

    Update: Exercises/SocialMedia

--- Exercises/SocialMedia/FriendFace.cs
+++ Exercises/SocialMedia/FriendFace.cs
@@ -4,8 +4,9 @@ public class FriendFace
 {
     private int _nextUserId = 0;
     private User _loggedInUser;
-    private List<User> _users;
+    private readonly List<User> _users;
     private bool isLoggedIn => _loggedInUser is not null;
+
     public FriendFace()
     {
         _loggedInUser = null;
@@ -17,7 +18,10 @@ public class FriendFace
         _loggedInUser = GetUser(userId);
     }
 
-    private User GetUser(int userId) => _users.First(val => val.Id == userId);
+    private User GetUser(int userId)
+    {
+        return _users.First(val => val.Id == userId);
+    }
 
     public int NewUser(string name, int age, string city)
     {
@@ -33,42 +37,115 @@ public class FriendFace
         }
     }
 
-    private int UpdateNextUserId() => _nextUserId++;
+    private void UpdateNextUserId()
+    {
+        _nextUserId++;
+    }
 
     public void StartMenu()
     {
-        Welcome();    
+        Welcome();
         if (!isLoggedIn)
         {
-            ErrorNotLoggedIn();
+            Console.WriteLine("User not logged in!");
             return;
         }
+
         LoggedIn();
         while (isLoggedIn)
         {
-            
+            MenuInfo();
+            if (Int32.TryParse(Console.ReadLine(), out int selection))
+            {
+                MenuSelection(selection);
+            }
         }
     }
 
     private void MenuSelection(int index)
     {
-        
+        switch (index)
+        {
+            case 1:
+                ViewUsers();
+                Console.Write("Select UserId: ");
+                var input = Console.ReadLine();
+                if (ValidateInput(input))
+                {
+                    _loggedInUser.AddFriend(Int32.Parse(input));
+                }
+
+                break;
+            case 2:
+                ViewUsers();
+                Console.Write("Select UserId: ");
+                var input2 = Console.ReadLine();
+                if (ValidateInput(input2))
+                {
+                    _loggedInUser.RemoveFriend(Int32.Parse(input2));
+                }
+
+                break;
+            case 3:
+                Console.WriteLine("Friends: ");
+                foreach (var friendId in _loggedInUser.FriendIds)
+                {
+                    GetUser(friendId).WriteMinimalInfo();
+                }
+
+                break;
+            case 4:
+                Console.WriteLine("Friends: ");
+                foreach (var friendId in _loggedInUser.FriendIds)
+                {
+                    GetUser(friendId).WriteMinimalInfo();
+                }
+
+                Console.Write("Select Friend Id: ");
+                var input3 = Console.ReadLine();
+                if (ValidateInput(input3))
+                {
+                    GetUser(Int32.Parse(input3)).WriteInfo();
+                }
+
+                break;
+        }
     }
 
-    private void ErrorNotLoggedIn()
+    private bool ValidateInput(string? validateInput)
     {
-        Console.WriteLine("User not logged in!");
+        return Int32.TryParse(validateInput, out int _);
+    }
+
+    private void MenuInfo()
+    {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1: Legg til bruker som venn");
+        Console.WriteLine("2: Fjern bruker som venn");
+        Console.WriteLine("3: Venner");
+        Console.WriteLine("4: Info fra venn");
+        Console.WriteLine();
+    }
+
+
+    private void ViewUsers()
+    {
+        foreach (var user in _users)
+        {
+            user.WriteMinimalInfo();
+        }
     }
 
     private void LoggedIn()
     {
         Console.WriteLine("UserInfo:");
         _loggedInUser.WriteInfo();
+        Console.WriteLine();
     }
 
     private void Welcome()
     {
         Console.WriteLine("Welcome to Friend Face");
+        Console.WriteLine();
     }
-
 }
--- Exercises/SocialMedia/User.cs
+++ Exercises/SocialMedia/User.cs
@@ -2,40 +2,45 @@ namespace SocialMedia;
 
 public class User
 {
-    private readonly int _id;
     private readonly string _name;
     private readonly int _age;
     private readonly string _city;
-    private List<int> friendIds;
 
-    public int Id => _id;
+    public int Id { get; }
+    public List<int> FriendIds { get; }
 
     public User(int id, string name, int age, string city)
     {
-        _id = id;
+        Id = id;
         _name = name;
         _age = age;
         _city = city;
+        FriendIds = new List<int>();
     }
 
     public void AddFriend(int userId)
     {
-        if (friendIds.Contains(userId)) return;
-        friendIds.Add(userId);
+        if (FriendIds.Contains(userId)) return;
+        FriendIds.Add(userId);
     }
-
+    
     public void RemoveFriend(int userId)
     {
-        var friendNotFound = !friendIds.Contains(userId);
+        var friendNotFound = !FriendIds.Contains(userId);
         if (friendNotFound) return;
-        friendIds.Remove(userId);
+        FriendIds.Remove(userId);
     }
     
     public void WriteInfo()
     {
-        Console.WriteLine($"Id: {_id}");
+        Console.WriteLine($"Id: {Id}");
         Console.WriteLine($"Name: {_name}");
         Console.WriteLine($"Age: {_age}");
         Console.WriteLine($"City: {_city}");
+    }
+
+    public void WriteMinimalInfo()
+    {
+        Console.WriteLine($"{Id}: {_name}");
     }
 }
